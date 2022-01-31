a9f1b400d0862d03b55211af5d538036f76caf3c
Sofia <hanziypetro@gmail.com>
2022-01-31 20:22:57 +0200

    timer

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,7 @@
-'use strict';
-const { ContextReplacementPlugin } = require("webpack");
+'use strict'; 
+window.addEventListener('DOMContentLoaded', () => {
 
- // ТАБИ
-document.addEventListener('DOMContentLoaded', () => {
-
-
+// ТАБИ
    const tabs = document.querySelectorAll('.tabheader__item'),
          tabsContent = document.querySelectorAll('.tabcontent'),
          tabsParent = document.querySelector('.tabheader__items  ');
@@ -25,7 +22,6 @@ document.addEventListener('DOMContentLoaded', () => {
       tabsContent[i].classList.add('show', 'fade');
       tabsContent[i].classList.remove('hide');
       tabs[i].classList.add('tabheader__item_active');
-   
    }
    showTabContent();
    hideTabContent();
@@ -46,5 +42,56 @@ document.addEventListener('DOMContentLoaded', () => {
        
    });
 
+// Timer
+const deadline = '2022-05-11';
+
+function getTimeRemaining(endtime) {
+   const t = Date.parse(endtime) - Date.parse(new Date()),
+         days = Math.floor(t / (1000 * 60 * 60 * 24)),
+         hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+         minutes = Math.floor((t / 1000 / 60 ) % 60),
+         seconds = Math.floor((t / 1000) % 60);
+
+   return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+   };
+}
+
+function getZiro(num) {
+   if (num >= 0 && num < 10) {
+      return `0${num}`;
+   } else {
+      return num;
+   }
+}
+
+function setClock(selector, endtime) {
+   const timer = document.querySelector(selector),
+         days = timer.querySelector('#days'),
+         hours = timer.querySelector('#hours'),
+         minutes = timer.querySelector('#minutes'),
+         seconds = timer.querySelector('#seconds'),
+         tinerInterval = setInterval(updateClock, 1000);
+   
+   updateClock();
+  
+   function updateClock () {
+      const t = getTimeRemaining(endtime);
+
+      days.innerHTML = getZiro(t.days);
+      hours.innerHTML = getZiro(t.hours);
+      minutes.innerHTML = getZiro(t.minutes);
+      seconds.innerHTML = getZiro(t.seconds);
+
+      if (t.total <= 0) {
+         clearInterval(tinerInterval); 
+      }
+   }
+}
+setClock('.timer', deadline);
 
 });
